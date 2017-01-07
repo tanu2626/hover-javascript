d1db7b4ef20f41ff1160235dcb33c93e1b9aeb86
Tanu <tanuuu.21@gmail.com>
2017-01-07 20:17:35 +1100

    Added css files

--- todo.js
+++ todo.js
@@ -1,9 +1,15 @@
-var firstLi = document.querySelector("li");
+var lis = document.querySelectorAll("li");
 
-firstLi.addEventListener("mouseover", function () {
-  firstLi.style.color ="green";
-});
+for(var i = 0; i < lis.length; i++){
+  lis[i].addEventListener("mouseover", function () {
+  this.classList.add("selected");
+  });
 
-firstLi.addEventListener("mouseout", function () {
-  firstLi.style.color ="black";
-});
+  lis[i].addEventListener("mouseout", function () {
+  this.classList.remove("selected");
+  });
+
+  lis[i].addEventListener("click", function () {
+    this.classList.toggle("done");
+  });
+}
