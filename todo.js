fab577a0fd4a0a52a0d64fe2a8ca88248773cb32
Tanu <tanuuu.21@gmail.com>
2017-01-07 20:07:06 +1100

    Add mouseover event listeners

---
+++ todo.js
@@ -0,0 +1,9 @@
+var firstLi = document.querySelector("li");
+
+firstLi.addEventListener("mouseover", function () {
+  firstLi.style.color ="green";
+});
+
+firstLi.addEventListener("mouseout", function () {
+  firstLi.style.color ="black";
+});
